fda656d7e53e7322712f11393fe97d2f625462f8
ramvilasreddy18-crypto <ramvilasreddy18@gmail.com>
2026-08-18 21:13:53 +0530

    solved aggrisive cows

---
+++ 2D_ARRAYS/aggresive_cows.py
@@ -0,0 +1,47 @@
+# brute force solution
+# arr = [0,3,4,7,9,10]
+# arr.sort()
+# cows = 4
+# def can_we_place(arr,dist,cows):
+#     countcows = 1
+#     lastcow = arr[0]
+#     for i in range(1,len(arr)):
+#         if arr[i]-lastcow>=dist:
+#             countcows += 1
+#             lastcow = arr[i]
+#         if countcows>=cows:
+#             return True
+#     return False
+# def aggresive_cows(arr,cows):
+#     for i in range(1,max(arr)-min(arr)+1):
+#         if (can_we_place(arr,i,cows)):
+#             continue
+#         else:
+#             return i-1
+# print(aggresive_cows(arr,cows))
+# optimal solution
+arr = [0,3,4,7,9,10]
+cows = 4
+def can_we_place(arr,dist,cows):
+    countcows = 1
+    lastcow = arr[0]
+    for i in range(1,len(arr)):
+        if arr[i]-lastcow>=dist:
+            countcows += 1
+            lastcow = arr[i]
+        if countcows>=cows:
+            return True
+    return False
+def aggresive_cows(arr,cows):
+    arr.sort()
+    low = 0
+    n = len(arr)
+    high = arr[n-1]-arr[0]
+    while low<=high:
+        mid = (low+high)//2
+        if can_we_place(arr,mid,cows):
+            low = mid+1
+        else:
+            high = mid-1
+    return high
+print(aggresive_cows(arr,cows))
